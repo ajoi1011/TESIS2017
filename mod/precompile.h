f8eb2131697760f08dbd6937b4bf70e7e38ffa2c
ajoi1011 <angeloramas87@gmail.com>
2020-06-14 20:22:52 -0400

    Create precompile.h

---
+++ mod/precompile.h
@@ -0,0 +1,14 @@
+/*
+ * precompile.h
+ *
+ * Sources files for Project
+ *
+ * Precompiled header generation file.
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+#include <ptlib.h>
+#include <ptclib/httpsvc.h>
+
+// End of File ///////////////////////////////////////////////////////////////
