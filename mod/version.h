497499e592df050cdaa0503102a32354cae31d99
ajoi1011 <angeloramas87@gmail.com>
2020-06-14 20:24:08 -0400

    Create version.h

---
+++ mod/version.h
@@ -0,0 +1,14 @@
+
+#ifndef _OpenMCU_VERSION_H
+#define _OpenMCU_VERSION_H
+
+#define MAJOR_VERSION 3
+#define MINOR_VERSION 48
+#define BUILD_TYPE    ReleaseCode
+#define BUILD_NUMBER  2
+
+
+#endif  // _OpenMCU_VERSION_H
+
+
+// End of File ///////////////////////////////////////////////////////////////
