6b0fe4699556cd67770e52466309293722692447
ajoi1011 <angeloramas87@gmail.com>
2021-05-17 17:24:06 +0000

    Update version.h

--- version.h
+++ version.h
@@ -1,7 +1,32 @@
 /*
  * version.h
- *
+ * 
+ * Copyright 2020 ajoi1011 <ajoi1011@debian>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
  */
+/*************************************************************************/
+/*************************************************************************/
+/*                                                                       */
+/*                       VERSION DEL SERVIDOR                            */
+/*                                                                       */
+/*************************************************************************/
+/*************************************************************************/
 
 #ifndef _OPAL_SERVER_VERSION_H
 #define _OPAL_SERVER_VERSION_H
@@ -11,13 +36,11 @@
    which extracts the version numbers
  *///ReleaseCode
 
-#define MAJOR_VERSION 1
+#define MAJOR_VERSION 2
 #define MINOR_VERSION 0
-#define BUILD_TYPE    BetaCode 
+#define BUILD_TYPE    ReleaseCode 
 #define BUILD_NUMBER 1
 
 
 #endif  // _OPAL_SERVER_VERSION_H
-
-
 // End of File ///////////////////////////////////////////////////////////////
