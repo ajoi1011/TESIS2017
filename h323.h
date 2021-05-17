98b3a1502ea6765419ba0cfc37c6a2cf6215792d
ajoi1011 <angeloramas87@gmail.com>
2021-05-17 17:08:10 +0000

    Update h323.h

--- h323.h
+++ h323.h
@@ -1,3 +1,26 @@
+/*
+ * h323.h
+ * 
+ * Copyright 2020 ajoi1011 <ajoi1011@debian>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
 /*************************************************************************/
 /*************************************************************************/
 /*                                                                       */
@@ -9,132 +32,38 @@
 #ifndef _H323_H
 #define _H323_H
 
-#include "manager.h"
 #include "precompile.h"
-
-#if OPAL_H323
-class MyGatekeeperServer;
-/*************************************************************************/
-/*                                                                       */
-/* <clase MyGatekeeperCall>                                              */
-/*                                                                       */
-/* <Descripción>                                                         */
-/*   Clase derivada de H323GatekeeperCall que describe una llamada       */
-/*  activa en un gatekeeper.                                             */
-/*                                                                       */
-/*************************************************************************/
-class MyGatekeeperCall : public H323GatekeeperCall                       //
-{                                                                        //
-  PCLASSINFO(MyGatekeeperCall, H323GatekeeperCall);                      //
-  public:                                                                //
-    MyGatekeeperCall(                                                    //
-      MyGatekeeperServer & server,                                       //
-      const OpalGloballyUniqueID & callIdentifier,                       //
-      Direction direction                                                //
-    );                                                                   //
-    ~MyGatekeeperCall();                                                 //
-                                                                         //
-    virtual H323GatekeeperRequest::Response OnAdmission(                 //
-      H323GatekeeperARQ & request                                        //
-    );                                                                   //
-                                                                         //
-};                                                                       //
-/*************************************************************************/
+class Manager;
 
 /*************************************************************************/
 /*                                                                       */
-/* <clase MyGatekeeperServer>                                            */
-/*                                                                       */
-/* <Descripción>                                                         */
-/*   Clase derivada de H323GatekeeperServer que describe un servidor     */
-/*  gatekeeper básico.                                                   */
-/*                                                                       */
-/*************************************************************************/
-class MyGatekeeperServer : public H323GatekeeperServer                   //
-{                                                                        //
-  PCLASSINFO(MyGatekeeperServer, H323GatekeeperServer);                  //
-  public:                                                                //
-    MyGatekeeperServer(H323EndPoint & ep);                               //
-                                                                         //
-    virtual H323GatekeeperCall * CreateCall(                             //
-      const OpalGloballyUniqueID & callIdentifier,                       //
-      H323GatekeeperCall::Direction direction                            //
-    );                                                                   //
-    virtual PBoolean TranslateAliasAddress(                              //
-      const H225_AliasAddress & alias,                                   //
-      H225_ArrayOf_AliasAddress & aliases,                               //
-      H323TransportAddress & address,                                    //
-      PBoolean & isGkRouted,                                             //
-      H323GatekeeperCall * call                                          //
-    );                                                                   //
-    bool Configure(PConfig & cfg, PConfigPage * rsrc);                   //
-    bool ForceUnregister(const PString id);                              //
-    PString OnLoadEndPointStatus(const PString & htmlBlock);             //
-                                                                         //
-  private:                                                               //
-    H323EndPoint & endpoint;                                             //
-                                                                         //
-    class RouteMap : public PObject                                      //
-    {                                                                    //
-      PCLASSINFO(RouteMap, PObject);                                     //
-      public:                                                            //
-        RouteMap(const PString & alias, const PString & host);           //
-        const H323TransportAddress & GetHost() const { return m_host; }  //
-        bool IsMatch(const PString & alias) const;                       //
-        bool IsValid() const;                                            //
-        void PrintOn(ostream & strm) const;                              //
-                                                                         //
-      private:                                                           //
-        PString              m_alias;                                    //
-        PRegularExpression   m_regex;                                    //
-        H323TransportAddress m_host;                                     //
-    };                                                                   //
-    PList<RouteMap> routes;                                              //
-    PDECLARE_MUTEX(reconfigurationMutex);                                //
-};                                                                       //
-/*************************************************************************/
-
-/*************************************************************************/
-/*                                                                       */
-/* <clase MyH323EndPoint>                                                */
+/* <clase MCUH323EndPoint>                                               */
 /*                                                                       */
 /* <Descripción>                                                         */
 /*   Clase derivada de H323ConsoleEndPoint que describe un terminal      */
 /*  H.323.                                                               */
 /*                                                                       */
 /*************************************************************************/
-class MyH323EndPoint : public H323ConsoleEndPoint                        //
-{                                                                        //
-  PCLASSINFO(MyH323EndPoint, H323ConsoleEndPoint);                       //
-  public:                                                                //
-    MyH323EndPoint(MyManager & manager);                                 //
-                                                                         //
-    virtual bool Initialise(                                             //
-      PArgList & args,                                                   //
-      bool verbose,                                                      //
-      const PString & defaultRoute                                       //
-    );                                                                   //
-    void AutoRegister(                                                   //
-      const PString & alias,                                             //
-      const PString & gk,                                                //
-      bool registering                                                   //
-    );                                                                   //
-    bool Configure(PConfig & cfg, PConfigPage * rsrc);                   //
-    const MyGatekeeperServer & GetGatekeeperServer() const               //
-    {                                                                    //
-      return m_gkServer;                                                 //
-    }                                                                    //
-                                                                         //
-    MyGatekeeperServer & GetGatekeeperServer() { return m_gkServer; }    //
-                                                                         //
-  protected:                                                             //
-    MyManager        & m_manager;                                        //
-    MyGatekeeperServer m_gkServer;                                       //
-    bool               m_firstConfig;                                    //
-    PStringArray       m_configuredAliases;                              //
-    PStringArray       m_configuredAliasPatterns;                        //
-};                                                                       //
-/*************************************************************************/
-#endif // OPAL_H323
-#endif // _H323_H
-/************************Final del Header*********************************/
+class MCUH323EndPoint : public H323ConsoleEndPoint  
+{
+  PCLASSINFO(MCUH323EndPoint, H323ConsoleEndPoint); 
+  public:
+    MCUH323EndPoint(Manager & manager);
+    
+    virtual bool Initialise(
+      PArgList & args,
+      bool verbose,
+      const PString & defaultRoute
+    );
+    
+    bool Configure(PConfig & cfg);
+    
+  //protected:
+    Manager          & m_manager; 
+    //MyGatekeeperServer m_gkServer;
+    bool               m_firstConfig;
+    PStringArray       m_configuredAliases;
+    PStringArray       m_configuredAliasPatterns;
+    
+};  
+#endif
